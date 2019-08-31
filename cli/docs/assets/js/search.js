aa94ad0dca01adc1e065fa59fe27f865f4c09091
Robin Tregaskis <robin.tregaskis@mtechaccess.co.uk>
2019-08-31 11:02:59 +0100

    ignore generated docs

--- cli/docs/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"1":"External module"},"rows":[{"id":0,"kind":1,"name":"\"bin/cli\"","url":"modules/_bin_cli_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":1,"name":"\"lib/lib\"","url":"modules/_lib_lib_.html","classes":"tsd-kind-external-module"}]};
